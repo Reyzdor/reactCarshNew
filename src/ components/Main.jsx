618cb6da2f081cc41e7dc52b16adbda9644ad8e9
Reyzdor <m3f3dr0nbristleback@gmail.com>
2025-09-18 02:15:40 +0300

    fix

---
+++ src/ components/Main.jsx
@@ -0,0 +1,26 @@
+import '../Main.css'
+import TextMain from './TextMain'
+
+export default function Main() {
+    return (
+        <div id="container">
+            <div id="mountains"></div>
+            <div id="road"></div>
+            <div id="road-line"></div>
+            <div class="car-container">
+            <div class="car">
+                <div class="car-body"></div>
+                <div class="car-cabin"></div>
+                <div class="car-windshield"></div>
+                <div class="car-headlight car-headlight-left"></div>
+                <div class="car-headlight car-headlight-right"></div>
+                <div class="car-wheel car-wheel-front"></div>
+                <div class="car-wheel car-wheel-back"></div>
+                <div class="car-smoke"></div>
+                <div class="car-smoke"></div>
+            </div>
+        </div>
+        </div>
+        
+    )
+}
---
+++ src/ components/TextMain.jsx
@@ -0,0 +1,8 @@
+export default function TextMain() {
+    return (
+        <div id="container">
+        <h1 id="drive">Поездка в</h1>
+        <h1 id="future">будущее</h1>
+        </div>
+    )
+}
