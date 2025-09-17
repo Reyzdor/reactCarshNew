c1eb01ad926f06d816d95d1fa320a162b9e5f1f2
Reyzdor <m3f3dr0nbristleback@gmail.com>
2025-09-17 19:55:04 +0300

    fix

--- src/ components/Header.jsx
+++ src/ components/Header.jsx
@@ -1,6 +1,7 @@
 import  Button  from './Button'
 import ButtonSign from './ButtonSing'
-
+import searchIcon from '/Users/roman/Desktop/reactAndVite/vite-project/src/assets/search1.svg'
+import carImage from '/Users/roman/Desktop/reactAndVite/vite-project/src/assets/car.png'
 
 export default function Header() {
     return (
@@ -8,12 +9,39 @@ export default function Header() {
                 <header>
                     <div id="container-inner">
                         <div id="area">
-                            <img src="" alt="" id="car" />
+                            <img src={carImage} alt="" style={{
+                                zIndex: '1',
+                                width: '30px',
+                                height: '30px',
+                                position: 'relative',
+                                top: '8px'
+                            }} />
                         </div>
                         <span id="logo">CarNow</span>
-                        <input id='input' 
-                        type="text" 
-                        placeholder="Поиск автомобилей по локации, модель..."/>
+                        <div style={{position: 'relative', display: 'inline-block'}}>
+                        <input 
+                            id='input' 
+                            type="text" 
+                            placeholder="Поиск автомобилей по локации, модель..."
+                            style={{paddingLeft: '40px',
+                                    width: '450px'
+                            }} 
+                        />
+                        <img 
+                            src={searchIcon} 
+                            alt="search" 
+                            style={{
+                                position: 'absolute',
+                                left: '12px',
+                                top: '45%',
+                                transform: 'translateY(-50%)',
+                                width: '15px',
+                                height: '15px',
+                                color: '#888',
+                                pointerEvents: 'none' 
+                            }} 
+                        />
+                    </div>
                         <div id="button-container">
                         <ButtonSign />
                         <Button />
