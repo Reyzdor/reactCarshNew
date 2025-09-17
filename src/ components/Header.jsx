e69146f8e7ac59610e1ff682260fdd0bc81b8950
Reyzdor <m3f3dr0nbristleback@gmail.com>
2025-09-17 17:48:26 +0300

    fix

--- src/ components/Header.jsx
+++ src/ components/Header.jsx
@@ -1,15 +1,23 @@
 import  Button  from './Button'
 import ButtonSign from './ButtonSing'
 
+
 export default function Header() {
     return (
             <div id="container">
                 <header>
                     <div id="container-inner">
+                        <div id="area">
+                            <img src="" alt="" id="car" />
+                        </div>
                         <span id="logo">CarNow</span>
-                        <input id='input' type="text" placeholder="Поиск автомобилей по локации, модель..." />
+                        <input id='input' 
+                        type="text" 
+                        placeholder="Поиск автомобилей по локации, модель..."/>
+                        <div id="button-container">
                         <ButtonSign />
                         <Button />
+                        </div>
                     </div>
                 </header>
             </div>
